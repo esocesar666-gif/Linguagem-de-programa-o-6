9ccc6a8b0a6e3434efb8fd505c6ed97c72658fdd
esocesar666-gif <esocesar666@gmail.com>
2026-04-16 12:13:16 -0300

    Update aula6ex6.c

--- aula6ex6.c
+++ aula6ex6.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 
 float juros_compostos(float capital, float taxa, int tempo) {
+    float capital_inicial = capital;
     
     while (0 < tempo) {
         capital = capital * (1 + taxa / 100);
         tempo--;
     }
       
-    return capital;
+    return capital - capital_inicial;
 }
     
 int main () {
-    float capital, taxa, tempo, total;
-    
+    float capital, taxa, total;
+    int tempo;
+   
     printf("Digite o valor investido: ");
     scanf("%f", &capital);
     printf("Digite a taxa de juros: ");
     scanf("%f", &taxa);
     printf("Digite o tempo: ");
-    scanf("%f", &tempo);
+    scanf("%d", &tempo);
     
     total = juros_compostos(capital, taxa, tempo);
     
-    printf("Total: %.2f", total);
+    printf("Total de juros compostos recebidos: %.2f", total);
     
     return 0;
 }
